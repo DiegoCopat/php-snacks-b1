bfbf9f4ee83ad46c62183c431438d5a22e84484d
DiegoCopat <diegocopat@gmail.com>
2021-06-28 17:11:03 +0200

    tentativi nell'esercizio 3

--- php-snack-2/index.php
+++ php-snack-2/index.php
@@ -1,5 +1,6 @@
 <?php 
-; ?>
+
+?>
 
 
 <!DOCTYPE html>
@@ -12,5 +13,22 @@
 </head>
 <body>
     
+    <!-- 
+    ISTRUZIONI PER IL PROGETTO
+
+    ## Snack 2
+
+    Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+     -->
+    
+    <h1> 
+
+        <?php 
+            
+
+        ?>
+
+    </h1>
 </body>
 </html>
---
+++ php-snack-3/index.php
@@ -0,0 +1,41 @@
+<?php
+
+    
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+<body>
+    
+    <!-- 
+    ISTRUZIONI PER IL PROGETTO
+
+    ## Snack 3
+    Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+    
+     -->
+    
+    <h1> 
+
+        <?php 
+            $Numeri = [];
+            for (count($Numeri) = 0; count($Numeri) < 15; $i++) {
+                
+                $NumCasuale = rand(1, 100);
+                $Numeri.push("$NumCasuale");
+                
+            }
+            var_dump($Numeri);
+        ?>
+
+    </h1>
+</body>
+</html>
